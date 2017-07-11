914fed5648639effd7be15aa9bc4790bff360ddc
Trevor Lemeron <trevorrl@me.com>
2017-07-11 11:02:30 -0400

    initial commit

---
+++ resources/views/social.blade.php
@@ -0,0 +1,94 @@
+<!doctype html>
+<html lang="{{ app()->getLocale() }}">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>Welcome to the Trev Zone</title>
+
+    <!-- Fonts -->
+    <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
+
+    <!-- Styles -->
+    <style>
+        html, body {
+            background-color: #fff;
+            color: #636b6f;
+            font-family: 'Raleway', sans-serif;
+            font-weight: 100;
+            height: 100vh;
+            margin: 0;
+        }
+
+        .full-height {
+            height: 100vh;
+        }
+
+        .flex-center {
+            align-items: center;
+            display: flex;
+            justify-content: center;
+        }
+
+        .position-ref {
+            position: relative;
+        }
+
+        .top-right {
+            position: absolute;
+            right: 10px;
+            top: 18px;
+        }
+
+        .content {
+            text-align: center;
+        }
+
+        .title {
+            font-size: 84px;
+        }
+
+        .links > a {
+            color: #636b6f;
+            padding: 0 25px;
+            font-size: 12px;
+            font-weight: 600;
+            letter-spacing: .1rem;
+            text-decoration: none;
+            text-transform: uppercase;
+        }
+
+        .m-b-md {
+            margin-bottom: 30px;
+        }
+        .title-image {
+            max-width: 100%;
+            max-height: 100%;
+        }
+    </style>
+</head>
+<body>
+<div class="flex-center position-ref full-height">
+
+    <div class="content">
+        <div class="links">
+            <a href="http://trevzone.dev">Go Back</a>
+        </div>
+        <div class="title-image">
+            <a href="http://imgur.com/LW9vhyN"><img src="http://i.imgur.com/LW9vhyN.png?1" title="source: imgur.com" width="256" height="256"/></a>
+        </div>
+        <div class="title m-b-md">
+            Trev Zone
+        </div>
+
+        <div class="links">
+            <a href="http://www.facebook.com/1708024242546113" target="_blank">Facebook</a>
+            <a href="https://twitter.com/trevxor" target="_blank">Twitter</a>
+            <a href="http://www.snapchat.com/add/trevxor" target="_blank">Snapchat</a>
+            <a href="http://instagram.com/trevxor" target="_blank">Instagram</a>
+        </div>
+    </div>
+</div>
+</body>
+</html>
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,91 @@
+<!doctype html>
+<html lang="{{ app()->getLocale() }}">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Welcome to the Trev Zone</title>
+
+        <!-- Fonts -->
+        <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
+
+        <!-- Styles -->
+        <style>
+            html, body {
+                background-color: #fff;
+                color: #636b6f;
+                font-family: 'Raleway', sans-serif;
+                font-weight: 100;
+                height: 100vh;
+                margin: 0;
+            }
+
+            .full-height {
+                height: 100vh;
+            }
+
+            .flex-center {
+                align-items: center;
+                display: flex;
+                justify-content: center;
+            }
+
+            .position-ref {
+                position: relative;
+            }
+
+            .top-right {
+                position: absolute;
+                right: 10px;
+                top: 18px;
+            }
+
+            .content {
+                text-align: center;
+            }
+
+            .title {
+                font-size: 84px;
+            }
+
+            .links > a {
+                color: #636b6f;
+                padding: 0 25px;
+                font-size: 12px;
+                font-weight: 600;
+                letter-spacing: .1rem;
+                text-decoration: none;
+                text-transform: uppercase;
+            }
+
+            .m-b-md {
+                margin-bottom: 30px;
+            }
+            .title-image {
+                max-width: 100%;
+                max-height: 100%;
+            }
+        </style>
+    </head>
+    <body>
+        <div class="flex-center position-ref full-height">
+
+            <div class="content">
+                <div class="title-image">
+                    <a href="http://imgur.com/LW9vhyN"><img src="http://i.imgur.com/LW9vhyN.png?1" title="source: imgur.com" width="256" height="256"/></a>
+                </div>
+                <div class="title m-b-md">
+                    Trev Zone
+                </div>
+
+                <div class="links">
+                    <a href="http://www.amazon.com/gp/registry/wishlist/3D8WYL9H0HOIF?reveal=purchased&tag=trevzone-20" target="_blank">My Setup</a>
+                    <a href="http://twitch.tv/trevxor" target="_blank">My Channel</a>
+                    <a href="http://trevzone.dev/social">Social Media</a>
+                    <a href="http://discord.gg/JwdKW7K" target="_blank">Discord</a>
+                </div>
+            </div>
+        </div>
+    </body>
+</html>

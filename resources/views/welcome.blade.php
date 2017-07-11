4a4eebfb5f165376061562d2b715499adc28f8b0
Trevor Lemeron <trevorrl@me.com>
2017-07-11 15:31:50 -0400

    changed page titles to "Good 'nuff"

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,7 +5,7 @@
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Welcome to the Trev Zone</title>
+        <title>Good 'nuff</title>
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">

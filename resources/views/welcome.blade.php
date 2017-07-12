b40391c609c4305d804879ae4f85c29c91326c54
Trevor Lemeron <trevorrl@me.com>
2017-07-12 11:05:50 -0400

    created common.css, moved styling into there, as well as made a few minor styling changes to better suit multiple viewports

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -8,67 +8,10 @@
         <title>Good 'nuff</title>
 
         <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
+        <link href="https://fonts.googleapis.com/css?family=Kalam:400,700" rel="stylesheet" type="text/css">
 
         <!-- Styles -->
-        <style>
-            html, body {
-                background-color: #fff;
-                color: #636b6f;
-                font-family: 'Raleway', sans-serif;
-                font-weight: 100;
-                height: 100vh;
-                margin: 0;
-            }
-
-            .full-height {
-                height: 100vh;
-            }
-
-            .flex-center {
-                align-items: center;
-                display: flex;
-                justify-content: center;
-            }
-
-            .position-ref {
-                position: relative;
-            }
-
-            .top-right {
-                position: absolute;
-                right: 10px;
-                top: 18px;
-            }
-
-            .content {
-                text-align: center;
-            }
-
-            .title {
-                font-size: 350%;
-            }
-
-            .links > a {
-                color: #636b6f;
-                padding: 0 15px;
-                font-size: 120%;
-                font-weight: 600;
-                letter-spacing: .1rem;
-                text-decoration: none;
-                text-transform: uppercase;
-            }
-
-            .m-b-md {
-                margin-bottom: 30px;
-            }
-            .title-image {
-                min-width: 100%;
-                min-height: 100%;
-                max-width: 100%;
-                max-height: 100%;
-            }
-        </style>
+        <link href="{{asset('css/common.css')}}" rel="stylesheet">
     </head>
     <body>
         <div class="flex-center position-ref full-height">

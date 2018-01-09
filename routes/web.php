0404d6d550e6aa055e8fc29e339e9468d0e0c9ef
Trevor Lemeron <trevorrl@me.com>
2018-01-09 07:03:56 -0500

    re styled home page with animated dropdowns on hover removed uneccessary blades removed unnecessary routes

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,12 @@
 |
 */
 
+Route::get('test', function () {
+
+    return view('test');
+
+});
+
 Route::get('/', function () {
 
     return view('home');
@@ -44,49 +50,5 @@ Route::get('tips', function () {
 Route::get('fidget', function () {
 
     return redirect('https://itunes.apple.com/us/app/animated-fidget-spinner-sticker-pack/id1272739329?mt=8');
-
-});
-
-Route::prefix('personal')->group(function () {
-
-    Route::get('setup', function () {
-
-        return view('setup');
-
-    });
-
-    Route::get('discord', function () {
-
-        return redirect('http://discord.gg/JwdKW7K');
-
-    });
-
-    Route::get('social', function () {
-
-        return view('social');
-
-    });
-
-    Route::get('home', function () {
-
-        return view('personal');
-
-    });
-
-});
-
-Route::prefix('professional')->group(function () {
-
-    Route::get('home', function () {
-
-        return view('professional');
-
-    });
-
-    Route::get('projects', function() {
-
-        return view('projects');
-
-    });
 
 });

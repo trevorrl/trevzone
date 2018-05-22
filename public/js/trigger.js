21702596f2d90b6cda2a71144968810b9b370a3a
Trevor Lemeron <trevor.lemeron@healthcareassetnetwork.com>
2018-05-22 15:38:38 -0400

    implemented streamlabs socket api for alerts triggering animation

---
+++ public/js/trigger.js
@@ -0,0 +1,167 @@
+window.onload = function () {
+
+    const streamlabs = io(('https://sockets.streamlabs.com?token=' + socketToken), {transports: ['websocket']});
+
+    let click     = document.getElementById('click');
+    let sliding   = document.getElementById('sliding');
+    let rotation  = document.getElementById('rotation');
+    let textalert = document.getElementById('textalert');
+    let logo      = document.getElementById('logo');
+    let msg1      = document.getElementById('msg1');
+    let msg2      = document.getElementById('msg2');
+
+    streamlabs.on('event', (eventData) => {
+        if (eventData.type === 'donation') {
+            //code to handle donation events
+            console.log("Donation received: ");
+            console.log(eventData);
+            msg1.textContent = (eventData.message[0].name + " just donated " + eventData.message[0].formatted_amount + "!");
+            msg2.textContent = (eventData.message[0].message);
+            sliding.classList.add('sliding-animate');
+            rotation.classList.add('rotation-animate');
+            textalert.classList.add('textalert-animate');
+            logo.classList.add('logo-animate');
+            console.log('animating');
+            setTimeout(function () {
+                sliding.classList.remove('sliding-animate');
+                rotation.classList.remove('rotation-animate');
+                textalert.classList.remove('textalert-animate');
+                logo.classList.remove('logo-animate');
+                msg1.textContent = "";
+                msg2.textContent = "";
+                console.log('animation finished, removing classes');
+            }, 12500);
+        }
+        if (eventData.for === 'twitch_account') {
+            switch(eventData.type) {
+                case 'follow':
+                    //code to handle follow events
+                    console.log("Follow received: ");
+                    console.log(eventData);
+                    msg1.textContent = (eventData.message[0].name + " just followed!")
+                    sliding.classList.add('sliding-animate');
+                    rotation.classList.add('rotation-animate');
+                    textalert.classList.add('textalert-animate');
+                    logo.classList.add('logo-animate');
+                    console.log('animating');
+                    setTimeout(function () {
+                        sliding.classList.remove('sliding-animate');
+                        rotation.classList.remove('rotation-animate');
+                        textalert.classList.remove('textalert-animate');
+                        logo.classList.remove('logo-animate');
+                        msg1.textContent = "";
+                        msg2.textContent = "";
+                        console.log('animation finished, removing classes');
+                    }, 12500);
+                    break;
+                case 'subscription':
+                    //code to handle subscription events
+                    console.log("Subscription received: ");
+                    console.log(eventData);
+                    msg1.textContent = (eventData.message[0].name + " just subscribed for " + eventData.message[0].months + " months!");
+                    msg2.textContent = (eventData.message[0].message);
+                    sliding.classList.add('sliding-animate');
+                    rotation.classList.add('rotation-animate');
+                    textalert.classList.add('textalert-animate');
+                    logo.classList.add('logo-animate');
+                    console.log('animating');
+                    setTimeout(function () {
+                        sliding.classList.remove('sliding-animate');
+                        rotation.classList.remove('rotation-animate');
+                        textalert.classList.remove('textalert-animate');
+                        logo.classList.remove('logo-animate');
+                        msg1.textContent = "";
+                        msg2.textContent = "";
+                        console.log('animation finished, removing classes');
+                    }, 12500);
+                    break;
+                case 'host':
+                    //code to handle host events
+                    console.log("Host received: ");
+                    console.log(eventData);
+                    msg1.textContent = (eventData.message[0].name + " just hosted with " + eventData.message[0].viewers + " viewers!");
+                    sliding.classList.add('sliding-animate');
+                    rotation.classList.add('rotation-animate');
+                    textalert.classList.add('textalert-animate');
+                    logo.classList.add('logo-animate');
+                    console.log('animating');
+                    setTimeout(function () {
+                        sliding.classList.remove('sliding-animate');
+                        rotation.classList.remove('rotation-animate');
+                        textalert.classList.remove('textalert-animate');
+                        logo.classList.remove('logo-animate');
+                        msg1.textContent = "";
+                        msg2.textContent = "";
+                        console.log('animation finished, removing classes');
+                    }, 12500);
+                    break;
+                case 'bits':
+                    //code to handle bits events
+                    console.log("Bits received: ");
+                    console.log(eventData);
+                    msg1.textContent = (eventData.message[0].name + " just dropped " + eventData.message[0].amount + " bits!");
+                    msg2.textContent = (eventData.message[0].message);
+                    sliding.classList.add('sliding-animate');
+                    rotation.classList.add('rotation-animate');
+                    textalert.classList.add('textalert-animate');
+                    logo.classList.add('logo-animate');
+                    console.log('animating');
+                    setTimeout(function () {
+                        sliding.classList.remove('sliding-animate');
+                        rotation.classList.remove('rotation-animate');
+                        textalert.classList.remove('textalert-animate');
+                        logo.classList.remove('logo-animate');
+                        msg1.textContent = "";
+                        msg2.textContent = "";
+                        console.log('animation finished, removing classes');
+                    }, 12500);
+                    break;
+                case 'raid':
+                    //code to handle raids events
+                    console.log("Raid received: ");
+                    console.log(eventData);
+                    msg1.textContent = (eventData.message[0].name + " just raided with " + eventData.message[0].raiders + " viewers!");
+                    sliding.classList.add('sliding-animate');
+                    rotation.classList.add('rotation-animate');
+                    textalert.classList.add('textalert-animate');
+                    logo.classList.add('logo-animate');
+                    console.log('animating');
+                    setTimeout(function () {
+                        sliding.classList.remove('sliding-animate');
+                        rotation.classList.remove('rotation-animate');
+                        textalert.classList.remove('textalert-animate');
+                        logo.classList.remove('logo-animate');
+                        msg1.textContent = "";
+                        msg2.textContent = "";
+                        console.log('animation finished, removing classes');
+                    }, 12500);
+                    break;
+                default:
+                    //default case
+                    console.log(eventData);
+            }
+        }
+    });
+
+    // test triggering animation and setting message text via click event
+    click.onclick = function (event) {
+
+        console.log('clicked, starting animation by adding classes');
+        msg1.textContent = "You clicked me!";
+        sliding.classList.add('sliding-animate');
+        rotation.classList.add('rotation-animate');
+        textalert.classList.add('textalert-animate');
+        logo.classList.add('logo-animate');
+        console.log('animating');
+        setTimeout(function () {
+            sliding.classList.remove('sliding-animate');
+            rotation.classList.remove('rotation-animate');
+            textalert.classList.remove('textalert-animate');
+            logo.classList.remove('logo-animate');
+            msg1.textContent = "";
+            console.log('animation finished, removing classes');
+        }, 12500);
+
+    }
+
+};
--- resources/views/labs.blade.php
+++ resources/views/labs.blade.php
@@ -5,29 +5,38 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <title>Good 'nuff</title>
+    <title>Trevzone Labs</title>
 
     <!-- Fonts -->
     <link href="https://fonts.googleapis.com/css?family=Permanent+Marker|Trocchi" rel="stylesheet" type="text/css">
 
     <!-- Styles -->
     <link href="{{asset('css/common.css')}}" rel="stylesheet">
+
+    <!-- Scripts -->
+    <script>
+        window.socketToken = "{{env('SOCKET_TOKEN')}}";
+    </script>
+    <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/socket.io/2.0.3/socket.io.js"></script>
+    <script type="text/javascript" src="{{asset('js/trigger.js')}}"></script>
+
 </head>
 <body class="per">
 <div class="flex-center position-ref full-height">
 
-    <div class="chroma">
-        <div class="sliding">
-            <div class="rotation">
-                <div class="textalert">
-                    <h1>Hello there!</h1>
-                </div>
-                <div class="logo">
-                    <img src="{{asset('logo.png')}}" title="logo" width="200" height="200"/>
+        <div id="click" class="click">
+            <div id="sliding" class="sliding">
+                <div id="rotation" class="rotation">
+                    <div id="textalert" class="textalert">
+                        <h1 id="msg1"></h1>
+                        <h2 id="msg2"></h2>
+                    </div>
+                    <div id="logo" class="logo">
+                        <img src="{{asset('logo.png')}}" title="logo" width="200" height="200"/>
+                    </div>
                 </div>
             </div>
         </div>
-    </div>
 
 </div>
 </body>
